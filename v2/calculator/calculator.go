eb0364374c9b0cbcab053fb9a4da83f5188614e9
francisco-serrano <francisco.serrano372@gmail.com>
2020-05-02 17:09:03 -0300

    adds v2

---
+++ v2/calculator/calculator.go
@@ -0,0 +1,26 @@
+package calculator
+
+func Add(values ...int) int {
+	sum := 0
+	for _, v := range values {
+		sum += v
+	}
+	return sum
+}
+
+func Sub(a, b int) int {
+	return a - b
+}
+
+func Mul(values ...int) int {
+	accumulator := 0
+	for _, v := range values {
+		accumulator *= v
+	}
+
+	return accumulator
+}
+
+func DoSomething() string {
+	return "something"
+}
---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/francisco-serrano/sample-library/v2
+
+go 1.14
